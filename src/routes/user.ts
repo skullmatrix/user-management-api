5cfdb999674f6c2013901d6ca0f2846c2087f691
unknown <MM542@ccs.local>
2025-03-24 18:58:13 +0800

    Add user creation functionality

---
+++ src/routes/user.ts
@@ -0,0 +1,22 @@
+import express from "express";
+import { createUser } from "../users/user.controller";
+import Joi from "joi";
+import { validateRequest } from "../_middleware/validate-request";
+
+const router = express.Router();
+
+// Create a new user
+router.post("/", createSchema, createUser);
+
+// Validation schema for user creation
+function createSchema(req: express.Request, res: express.Response, next: express.NextFunction) {
+    const schema = Joi.object({
+        firstName: Joi.string().required(),
+        lastName: Joi.string().required(),
+        email: Joi.string().email().required(),
+        section: Joi.string().required(),
+    });
+    validateRequest(req, res, next, schema);
+}
+
+export default router;
