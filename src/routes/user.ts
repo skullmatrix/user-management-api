563d1120da7161ff4eb8080d70ae009e81bdcaca
unknown <YY222@ccs.local>
2025-03-25 17:40:35 +0800

    Add user listing and retrieval functionality

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -1,28 +1,12 @@
 import express from "express";
-import Joi from "joi";
 import { getAllUsers, getUserById } from "../controllers/user.controller";
-import { updateUser, deleteUser } from "../controllers/user.controller";
-import { validateRequest } from "../_middleware/validate-request";
 
 const router = express.Router();
 
-// Update a user
-router.put("/:id", updateSchema, updateUser);
+// Get all users
+router.get("/", getAllUsers);
 
-// Delete a user
-router.delete("/:id", deleteUser);
-
-// Validation schema for user update
-function updateSchema(req: express.Request, res: express.Response, next: express.NextFunction) {
-    const schema = Joi.object({
-        firstName: Joi.string().empty(""),
-        lastName: Joi.string().empty(""),
-        email: Joi.string().email().empty(""),
-        section: Joi.string().empty(""),
-    });
-    validateRequest(req, res, next, schema);
-}
-
-
+// Get a user by ID
+router.get("/:id", getUserById);
 
 export default router;
